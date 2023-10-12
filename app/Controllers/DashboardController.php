c814db52c0a04b4560b3cba83204dc36dee1e241
Eric Mensah <ericmensah@codewrite.org>
2023-10-12 18:12:28 +0000

    updated user details and perms

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -2,18 +2,14 @@
 
 namespace App\Controllers;
 
-use CodeIgniter\HTTP\RequestInterface;
-use CodeIgniter\HTTP\ResponseInterface;
-use CodeIgniter\Shield\Authorization\Groups;
-use CodeIgniter\Shield\Entities\Group;
-use CodeIgniter\Shield\Models\GroupModel;
-use Psr\Log\LoggerInterface;
-
 class DashboardController extends BaseController
 {
 
     public function index(): string
     {
-        return view('dashboard');
+        $data = [
+            'title' => 'Dashboard',
+        ];
+        return view('dashboard', $data);
     }
 }
