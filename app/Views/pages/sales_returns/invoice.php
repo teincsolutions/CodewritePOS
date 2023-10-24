793130a14efd32fa07917aea5d3c2123d578eded
Eric Mensah <ericmensah@codewrite.org>
2023-10-24 10:50:14 +0000

    fixes

--- app/Views/pages/sales_returns/invoice.php
+++ app/Views/pages/sales_returns/invoice.php
@@ -194,7 +194,6 @@
                 </div>
                 <div class="col-lg-12">
                     <a href="javascript:void(0);" class="btn btn-submit me-2">Print Invoice</a>
-                    <a href="<?= site_url('sales/returns/create?invoice=' . $return->invoice) ?>" class="btn btn-submit me-2">Return</a>
                 </div>
             </div>
         </div>
