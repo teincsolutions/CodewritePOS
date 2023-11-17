9af73b57570bad3f2f5d40305d8aad16370f0cc2
Eric Mensah <ericmensah@codewrite.org>
2023-11-17 16:27:27 +0000

    fixes

--- public/assets/js/handle-adjustment.js
+++ public/assets/js/handle-adjustment.js
@@ -200,8 +200,8 @@ function autocomplete(inp) {
 
           b.innerHTML =
             item.discontinued == 1
-              ? `<span class="d-flex justify-content-between" style="z-index:1000"><del><code>${item.sku}</code> ${item.name}(${item.unit.label}) - <i>${info}</i></del>GHS ${item.unit_cost}</span>`
-              : `<span class="d-flex justify-content-between" style="z-index:1000"><span><code>${item.sku}</code> ${item.name}(${item.unit.label}) - <i>${info}</i></span>GHS ${item.unit_cost}</span>`;
+              ? `<span class="d-flex justify-content-between" style="z-index:1000"><del><code>${item.sku}</code> ${item.name}(${item.unit.label}) - <i>${info}</i></del>GHS ${item.unit_cost}|${item.unit_price}</span>`
+              : `<span class="d-flex justify-content-between" style="z-index:1000"><span><code>${item.sku}</code> ${item.name}(${item.unit.label}) - <i>${info}</i></span>GHS ${item.unit_cost}|${item.unit_price}</span>`;
 
           b.addEventListener("click", function (e) {
             let store = "";
