9d2532a3533c145f207af2958a348c250a48d537
Eric Mensah <ericmensah@codewrite.org>
2023-11-18 07:47:29 +0000

    updates

--- app/Views/pages/closing/list_closing.php
+++ app/Views/pages/closing/list_closing.php
@@ -15,12 +15,6 @@
         <div class="card-body">
             <div class="table-top">
                 <div class="search-set">
-                    <div class="search-path">
-                        <a class="btn btn-filter" id="filter_search">
-                            <img src="<?= base_url('assets/icons/filter.svg') ?>" alt="img">
-                            <span><img src="<?= base_url('assets/icons/closes.svg') ?>" alt="img"></span>
-                        </a>
-                    </div>
                     <div class="search-input">
                         <a class="btn btn-searchset">
                             <i class="fa fa-search"></i>
@@ -32,7 +26,7 @@
                 </div>
             </div>
 
-            <div class="card" id="filter_inputs">
+            <div class="card" id="filter_inputs9">
                 <div class="card-body pb-0">
                     <div class="row">
                         <div class="col-lg-2 col-sm-6 col-12">
@@ -42,14 +36,13 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-sm-6 col-12">
+                        <div class="col-lg-3 col-sm-6 col-12" style="overflow-x: auto;">
                             <div class="form-group">
                                 <select name="store_id" class="select2-store">
-                                    <option value=""></option>
                                     <?php
                                     if (isset($stores))
                                         foreach ($stores as $row) { ?>
-                                        <option value="<?= $row->id ?>">
+                                        <option value="<?= $row->id ?>" <?= ($row->id === $settings->get('App.DefaultStore', $context) ? 'selected' : '') ?>>
                                             <?= $row->name; ?> (<?= $row->location; ?>)
                                         </option>
                                     <?php } ?>
