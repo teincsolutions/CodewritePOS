9004c46b2402127306380c84f59be2d582c66a01
Eric Mensah <ericmensah@codewrite.org>
2023-12-03 16:14:08 +0000

    updates and fixes

---
+++ public/assets/js/datatables/overdue-sales-report.js
@@ -0,0 +1,185 @@
+let table;
+
+$(function () {
+  table = $("#dt-overdue-report").DataTable({
+    ajax: {
+      url: baseUrl + "reports/overdue/sales/datatable",
+      dataType: "json",
+      contentType: "application/json",
+      data: function (params) {
+        let filter = {};
+        let filterForm = $("#filter_inputs9 input, #filter_inputs9 select");
+        filterForm.each((i, item) => {
+          field = $(item);
+
+          if (field.prop("tagName") === "SELECT") {
+            if (
+              typeof field.children("option:selected").val() !== "undefined" &&
+              field.children("option:selected").val() != ""
+            )
+              filter[field.attr("name")] = field
+                .children("option:selected")
+                .val();
+          } else if (typeof field.attr("name") !== "undefined") {
+            filter[field.attr("name")] = field.val();
+          }
+        });
+        params.fields = filter;
+      },
+    },
+    bFilter: true,
+    dom: "fBtlpi",
+    buttons: [
+      {
+        extend: "print",
+        text: '<a data-bs-toggle="tooltip" data-bs-placement="top" title="print"><i class="fa text-secondary fa-print"></i></a>',
+        className: "btn btn-light",
+      },
+      "spacer",
+      {
+        extend: "excel",
+        text: '<a data-bs-toggle="tooltip" data-bs-placement="top" title="excel"><i class="fa text-success fa-file-excel"></i></a>',
+        className: "btn btn-light",
+      },
+      "spacer",
+      {
+        extend: "pdf",
+        text: '<a data-bs-toggle="tooltip" data-bs-placement="top" title="pdf"><i class="fa text-danger fa-file-pdf"></i></a>',
+        className: "btn btn-light",
+      },
+    ],
+    pagingType: "numbers",
+    ordering: true,
+    language: {
+      search: " ",
+      sLengthMenu: "_MENU_",
+      searchPlaceholder: "Search...",
+      info: "_START_ - _END_ of _TOTAL_ items",
+    },
+    columns: [
+      {
+        data: null,
+        render: function (data, type, row) {
+          return null;
+        },
+      },
+      {
+        data: "customer",
+        render: function (data) {
+          return `${data.name}`;
+        },
+      },
+      {
+        data: "customer",
+        render: function (data) {
+          return `${data.phone}`;
+        },
+      },
+      {
+        data: "customer",
+        render: function (data) {
+          return `${data.address ?? ""}`;
+        },
+      },
+      {
+        data: "tdate",
+      },
+      {
+        data: "total_due",
+        render: function (data) {
+          return `GHS ${parseFloat(data).toFixed(2)}`;
+        },
+      },
+      {
+        data: "customer",
+        render: function (data) {
+          return `${data.type.toUpperCase()}`;
+        },
+      },
+      {
+        data: "customer",
+        render: function (data) {
+          return data.balance < 0
+            ? `(GHS ${Math.abs(data.balance).toFixed(2)})`
+            : `GHS ${parseFloat(data.balance).toFixed(2)}`;
+        },
+      },
+      {
+        data: "customer",
+        render: function (data, type, row) {
+          if (type === "display") {
+            return `<div class="d-flex align-items-center">
+                        <a target="_blank" href="${baseUrl}/customers/${data.id}" class="me-3"><i class="fa fa-eye fa-lg"></i></a>
+                       </div>`;
+          }
+          return null;
+        },
+      },
+    ],
+    initComplete: (settings, json) => {
+      $(".dataTables_filter").appendTo("#tableSearch");
+      $(".dataTables_filter").appendTo(".search-input");
+      if ($('[data-bs-toggle="tooltip"]').length > 0) {
+        var tooltipTriggerList = [].slice.call(
+          document.querySelectorAll('[data-bs-toggle="tooltip"]')
+        );
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+          return new bootstrap.Tooltip(tooltipTriggerEl);
+        });
+      }
+
+      var selectAllItems = "#select-all";
+      var checkboxItem = ":checkbox";
+
+      $(selectAllItems).click(function () {
+        if (this.checked) {
+          $(checkboxItem).each(function () {
+            this.checked = true;
+          });
+        } else {
+          $(checkboxItem).each(function () {
+            this.checked = false;
+          });
+        }
+      });
+    },
+    footerCallback: function (row, data, start, end, display) {
+      var api = this.api();
+      // Remove the formatting to get integer data for summation
+      var intVal = function (i) {
+        return typeof i === "string"
+          ? i.replace(/[\$,]/g, "") * 1
+          : typeof i === "number"
+          ? i
+          : 0;
+      };
+    },
+    order: [[1, "desc"]],
+    columnDefs: [
+      {
+        orderable: false,
+        className: "select-checkbox",
+        targets: 0,
+      },
+    ],
+    select: {
+      style: "multi",
+      selector: "td:first-child",
+    },
+  });
+  table.buttons().container().appendTo(".wordset");
+
+  $(".filter").on("click select2:select select2:unselect", function (params) {
+    table.ajax.reload();
+  });
+
+  $(".filter-clear").on("click", function (params) {
+    $("#date-from,#date-to").val("");
+    table.ajax.reload();
+  });
+
+  $(".select2-store").select2({
+    placeholder: "Seach a store",
+    allowClear: true,
+  });
+});
