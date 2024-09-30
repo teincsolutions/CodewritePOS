38c6137fa277794912c1f94d71562912a9efe169
Eric Mensah <ericmensah@codewrite.org>
2024-09-30 18:23:50 +0000

    add scanner

--- app/Views/pages/sales/pos.php
+++ app/Views/pages/sales/pos.php
@@ -624,11 +624,11 @@
     </form>
     <?= $this->endSection() ?>
     <?= $this->section('script') ?>
-    <script src="<?= base_url('assets/js/handle-pos.js?v=33') ?>"></script>
+    <script src="<?= base_url('assets/js/handle-pos.js?v=34') ?>"></script>
     <script src="<?= base_url('assets/js/datatables/pos.modal.js?v=5') ?>"></script>
     <script src="<?= base_url('assets/js/record-actions.js') ?>"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/quagga/0.12.1/quagga.min.js"></script>
-    <script src="<?= base_url('assets/js/handle-scanner.js?v=0') ?>"></script>
+    <script src="<?= base_url('assets/js/handle-scanner.js?v=1') ?>"></script>
 
     <?php if (isset($sales) && $sales->customer) {
         $customer = $sales->customer;
