8e9875cd24e4a03b817e8e3a32182d61f82dfeb9
Eric Mensah <ericmensah@codewrite.org>
2024-10-22 14:36:30 +0000

    fixing supplier-details.js

--- app/Models/SalesModel.php
+++ app/Models/SalesModel.php
@@ -99,7 +99,7 @@ class SalesModel extends Model
                             ->where('sale_id', $row->id)
                             ->get()
                             ->getRowObject()->total;
-                        $model['data']->checkoutPaid = $model['data']->paid;
+                        $model['data'][$key]->checkoutPaid = $model['data']->paid;
                         $model['data'][$key]->paid = $total ?? 0.00;
                     }
                     $model['data'][$key]->store = $storeModel->where('id', $row->store_id)->first();
