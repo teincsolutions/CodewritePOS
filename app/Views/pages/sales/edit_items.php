9d5e86219f2b271acddc2168cba78046a31cffe3
Eric Mensah <eric.m4511@gmail.com>
2026-08-21 00:21:58 +0000

    Fix undefined variable / in edit_items views
    - Use isset() instead of direct variable reference in purchase/edit_items.php
    - Same fix for sales/edit_items.php
    - Views now safely handle cases where / may not be set

--- app/Views/pages/sales/edit_items.php
+++ app/Views/pages/sales/edit_items.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="editItemsModalLabel">Edit Sales Items - <?= $sale ? $sale->invoice : '' ?></h5>
+                <h5 class="modal-title" id="editItemsModalLabel">Edit Sales Items - <?= isset($sale) ? $sale->invoice : '' ?></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
